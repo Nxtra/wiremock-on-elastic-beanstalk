7df0dadd011d7bb8e3022bd88a72c48c88648109
Nxtra <nickvanhoof1@gmail.com>
2019-12-10 22:46:30 +0100

    add region and stackname as parameters for scripts

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 read -p "Enter aws profile, press enter for [default]: " name
-profile=${name:-default}
+PROFILE=${name:-default}
 
-# IMPORTANT: Bucket names must be unique for all AWS users.
-read -p "Enter bucketName, must be globally unique: " bucket
-BUCKET=${bucket:-wiremock-elastic-beanstalk}
+# IMPORTANT: Bucket names must be globally unique across all AWS users.
+BUCKET="wiremock-elastic-beanstalk"
+REGION="eu-west-1"
+STACKNAME="wiremock"
 
-echo "Deploying with parameters: profile=${profile}, bucket={$BUCKET}"
+echo "Deploying with parameters: profile=${PROFILE}, bucket=${BUCKET}"
 
 curl http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.25.1/wiremock-standalone-2.25.1.jar --output wiremock.jar
 
 # Create your deployment bucket if it doesn't exist yet.
-aws s3 mb s3://$BUCKET --profile $profile
+aws s3 mb s3://$BUCKET --profile $PROFILE
 
 # Create zip deployment package
 zip wiremock.zip wiremock.jar Procfile
@@ -23,5 +24,5 @@ aws s3api put-object --bucket wiremock-elastic-beanstalk --key wiremock.zip --bo
 
 # Create cloudformation stack
 echo "Creating stack"
-aws cloudformation create-stack --profile $profile --stack-name wiremock --template-body file://./template.yaml \
+aws cloudformation create-stack --profile $PROFILE --region $REGION --stack-name $STACKNAME --template-body file://./template.yaml \
 --parameters ParameterKey=S3Bucket,ParameterValue=$BUCKET ParameterKey=S3Key,ParameterValue=wiremock.zip
--- update.sh
+++ update.sh
@@ -3,12 +3,13 @@
 read -p "Enter aws profile, press enter for [default]: " name
 profile=${name:-default}
 
-
-read -p "Enter your bucketName: " bucket
-BUCKET=${bucket:-wiremock-elastic-beanstalk}
+# IMPORTANT: Bucket names must be globally unique across all AWS users.
+BUCKET="wiremock-elastic-beanstalk"
+REGION="eu-west-1"
+STACKNAME="wiremock"
 
 
 #update stack
 echo "Updating stack"
-aws cloudformation update-stack --profile $profile --stack-name wiremock --template-body file://./template.yaml \
+aws cloudformation update-stack --profile $profile --region $REGION --stack-name $STACKNAME --template-body file://./template.yaml \
 --parameters ParameterKey=S3Bucket,ParameterValue=$BUCKET ParameterKey=S3Key,ParameterValue=wiremock.zip
